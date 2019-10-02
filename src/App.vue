d4c8b89222516fa5602aabfcb0eacd11b6ca9aa4
MuhaddiMu <Muhaddisshah@gmail.com>
2019-10-03 02:57:02 +0500

    :bulb: Toggle Dark & Light Theme

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,41 @@
 <template>
-  <v-app>
+  <v-app :dark="setTheme">
     <v-app-bar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>Vueti</span>
+        <span class="font-weight-light">FORM</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
+      <v-btn fab text @click="ActivateLightTheme = !ActivateLightTheme">
+        <v-icon :color="`${!ActivateLightTheme && 'yellow'}`" :light="true">mdi-brightness-4</v-icon>
       </v-btn>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <TheForm />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import TheForm from './components/TheForm';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    TheForm,
   },
   data: () => ({
-    //
+    ActivateLightTheme: false
   }),
+  computed: {
+    setTheme() {
+      if (this.ActivateLightTheme == true) {
+        return (this.$vuetify.theme.dark = false);
+      } else {
+        return (this.$vuetify.theme.dark = true);
+      }
+    }
+  }
 };
 </script>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -5,6 +5,9 @@ import "vuetify/dist/vuetify.min.css"
 Vue.use(Vuetify)
 
 export default new Vuetify({
+  theme: {
+    dark: true
+  },
   icons: {
     iconfont: "mdi"
   }
