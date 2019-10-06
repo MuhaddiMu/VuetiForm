892feece859e2cfe758e225c259d9bed22433bf1
MuhaddiMu <Muhaddisshah@gmail.com>
2019-10-06 18:59:42 +0500

    :sparkles: Display Code Modal

--- src/components/TheFormResult.vue
+++ src/components/TheFormResult.vue
@@ -1,86 +1,109 @@
 <template>
-  <v-card class="mx-auto">
-    <v-card-title>Result 😎</v-card-title>
+  <div>
+    <v-card class="mx-auto">
+      <v-card-title>Result 😎</v-card-title>
 
-    <v-form ref="Form">
-      <v-container class="text-center">
-        <v-flex v-for="(Field, index) in Fields" :key="index">
-          <v-text-field
-            :dense="FormSettings.Dense === true"
-            :filled="FormSettings.InputStyle === 'Filled' ? true : false"
-            :outlined="FormSettings.InputStyle === 'Outlined' ? true : false"
-            :solo="FormSettings.InputStyle === 'Solo' ? true : false"
-            v-if="Field.Type === 'TextField' && Field.Rules !== 'Password'"
-            v-model="Field.DefaultVal"
-            :label="Field.Label"
-            :type="Field.Rules"
-            :min="0"
-            :counter="Field.Counter"
-            :rules="Rules(Field)"
-          ></v-text-field>
+      <v-form ref="Form">
+        <v-container class="text-center">
+          <v-flex v-for="(Field, index) in Fields" :key="index">
+            <v-text-field
+              :dense="FormSettings.Dense === true"
+              :filled="FormSettings.InputStyle === 'Filled' ? true : false"
+              :outlined="FormSettings.InputStyle === 'Outlined' ? true : false"
+              :solo="FormSettings.InputStyle === 'Solo' ? true : false"
+              v-if="Field.Type === 'TextField' && Field.Rules !== 'Password'"
+              v-model="Field.DefaultVal"
+              :label="Field.Label"
+              :type="Field.Rules"
+              :min="0"
+              :counter="Field.Counter"
+              :rules="Rules(Field)"
+            ></v-text-field>
 
-          <v-text-field
-            :dense="FormSettings.Dense === true"
-            :filled="FormSettings.InputStyle === 'Filled' ? true : false"
-            :outlined="FormSettings.InputStyle === 'Outlined' ? true : false"
-            :solo="FormSettings.InputStyle === 'Solo' ? true : false"
-            v-model="Field.DefaultVal"
-            v-if="Field.Rules === 'Password'"
-            :append-icon="Field.TogglePassword ? 'mdi-eye' : 'mdi-eye-off'"
-            :type="Field.TogglePassword ? 'text' : 'password'"
-            :label="Field.Label"
-            hint="At least 8 characters"
-            :counter="Field.Counter"
-            :rules="Rules(Field)"
-            @click:append="Field.TogglePassword = !Field.TogglePassword"
-          ></v-text-field>
+            <v-text-field
+              :dense="FormSettings.Dense === true"
+              :filled="FormSettings.InputStyle === 'Filled' ? true : false"
+              :outlined="FormSettings.InputStyle === 'Outlined' ? true : false"
+              :solo="FormSettings.InputStyle === 'Solo' ? true : false"
+              v-model="Field.DefaultVal"
+              v-if="Field.Rules === 'Password'"
+              :append-icon="Field.TogglePassword ? 'mdi-eye' : 'mdi-eye-off'"
+              :type="Field.TogglePassword ? 'text' : 'password'"
+              :label="Field.Label"
+              hint="At least 8 characters"
+              :counter="Field.Counter"
+              :rules="Rules(Field)"
+              @click:append="Field.TogglePassword = !Field.TogglePassword"
+            ></v-text-field>
 
-          <v-textarea
-            v-if="Field.Type == 'Textarea'"
-            :filled="Field.Rules === 'Filled' ? true : false"
-            :outlined="Field.Rules === 'Outlined' ? true : false"
-            :solo="Field.Rules === 'Solo' ? true : false"
-            :min="0"
-            :label="Field.Label"
-            :counter="Field.Counter"
-            :value="Field.DefaultVal"
-            :rules="Rules(Field)"
-          ></v-textarea>
-        </v-flex>
+            <v-textarea
+              v-if="Field.Type == 'Textarea'"
+              :filled="Field.Rules === 'Filled' ? true : false"
+              :outlined="Field.Rules === 'Outlined' ? true : false"
+              :solo="Field.Rules === 'Solo' ? true : false"
+              :min="0"
+              :label="Field.Label"
+              :counter="Field.Counter"
+              :value="Field.DefaultVal"
+              :rules="Rules(Field)"
+            ></v-textarea>
+          </v-flex>
 
-        <v-btn
-          v-if="Fields.length > 0"
-          :disabled="!Valid"
-          color="success"
-          class="mr-4"
-          @click="Validate"
-          >Validate</v-btn
+          <v-btn
+            v-if="Fields.length > 0"
+            :disabled="!Valid"
+            color="success"
+            class="mr-4"
+            @click="Validate"
+            >Validate</v-btn
+          >
+
+          <v-btn
+            v-if="Fields.length > 0"
+            color="error"
+            class="mr-4"
+            @click="Reset"
+            >Reset Form</v-btn
+          >
+          <v-btn
+            v-if="Fields.length > 0"
+            color="warning"
+            @click="ResetValidation"
+            >Reset Validation</v-btn
+          >
+        </v-container>
+      </v-form>
+      <v-card-actions>
+        <v-btn @click="Dialog = true" text>
+          Give Me Code
+          <v-icon>mdi-code-tags-check</v-icon>
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-dialog v-model="Dialog" max-width="700">
+      <v-card shaped>
+        <v-card-title class="headline"
+          >VuetiForm <a href="https://github.com/MuhaddiMu/VuetiForm" target="_blank" rel="noopener noreferrer" ><v-icon>mdi-github-circle</v-icon></v-card-title
         >
 
-        <v-btn
-          v-if="Fields.length > 0"
-          color="error"
-          class="mr-4"
-          @click="Reset"
-          >Reset Form</v-btn
-        >
-        <v-btn v-if="Fields.length > 0" color="warning" @click="ResetValidation"
-          >Reset Validation</v-btn
-        >
-      </v-container>
-    </v-form>
-    <v-card-actions>
-      <v-btn text>
-        Give Me Code
-        <v-icon>mdi-code-tags-check</v-icon>
-      </v-btn>
-    </v-card-actions>
-  </v-card>
+        <v-card-text> </v-card-text>
+
+        <v-card-actions>
+          <div class="flex-grow-1"></div>
+
+          <v-btn color="green darken-1" text @click="Dialog = false">
+            Copy to Clipboard <v-icon>mdi-content-copy</v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
 export default {
   data: () => ({
+    Dialog: false,
     Valid: true,
     Fields: [],
     FormSettings: null
